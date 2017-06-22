0ec2ff004266c3a89d5acf06e075fb59b28ee747
Vinicius <vinicius@syntesis.com.br>
2017-06-21 23:35:06 -0300

    Sign-up Form Added a simple sign-up form with a simple angular form validate.

--- account-manager/src/app/account-form.component.ts
+++ account-manager/src/app/account-form.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
-import { Account }    from './account';
+import { Account } from './account';
 
 @Component({
   selector: 'account-form',
   templateUrl: './account-form.component.html'
 })
+
 export class AccountFormComponent {
 
   model = new Account();
@@ -13,7 +14,4 @@ export class AccountFormComponent {
   submitted = false;
 
   onSubmit() { this.submitted = true; }
-
-  // TODO: Remove this when we're done
-  // get diagnostic() { return JSON.stringify(this.model); }
 }
--- account-manager/src/app/app.module.ts
+++ account-manager/src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
  
 import { AppComponent }  from './app.component';
+import { routing, routedComponents } from './app.routing';
 import { AccountFormComponent } from './account-form.component';
  
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing,
+    HttpModule,
+    JsonpModule
   ],
   declarations: [
     AppComponent,
+    routedComponents,
     AccountFormComponent
   ],
   bootstrap: [ AppComponent ]
